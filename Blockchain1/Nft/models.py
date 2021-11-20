bf030666509a116090f80ad4ee93f5ab1ce18c24
Harshsingh47 <77436645+Harshsingh47@users.noreply.github.com>
2021-11-20 18:27:34 +0530

    Add files via upload

---
+++ Blockchain1/Nft/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+from django.db import models
+from django.db import models
+
+class Contract(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
